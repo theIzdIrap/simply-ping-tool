cfb11eda3a57159f88b647c77645c49063522c0d
D3Ti <62066592+TheD3Ti@users.noreply.github.com>
2023-04-12 12:19:24 -0700

    Create ping.py

---
+++ ping.py
@@ -0,0 +1,49 @@
+from tkinter import *
+from tkinter import ttk
+import os
+
+win = Tk()
+
+
+#tk settings
+
+win.geometry("600x300")
+win.title("Ping Tool")
+win.resizable(False,False)
+
+
+#delete function
+def delete():
+    label3.destroy()
+
+
+
+#get function for get entry and ping
+
+def get_value():
+   global label2
+   global label3
+   global response
+   e_text=entry.get()
+   response = os.system("ping " + e_text)
+   if response == 0:
+       label3 = Label(win, text=str(e_text) + " is up", font=('Century 15 bold'))
+       label3.pack()
+   else:
+       label3 = Label(win, text=str(e_text) + " is down", font=('Century 15 bold'))
+       label3.pack()
+
+
+#entry settings
+
+entry = ttk.Entry(win,font=('Century 12'),width=40)
+entry.pack(pady= 30)
+
+
+#button settings
+
+button = ttk.Button(win, text="Enter", command= get_value)
+button.pack()
+button = ttk.Button(win, text="Delete", command= delete)
+button.pack()
+win.mainloop()
